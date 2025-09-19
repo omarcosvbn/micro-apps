56fb8e4a9bbd708f52260c1a2dbcc153bee37f5d
omarcosvbn <omarcosvbn@gmail.com>
2025-09-20 00:04:29 +0100

    hamburger menu

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { Doto } from 'next/font/google'
 import styles from './Header.module.scss'
+import { useState } from 'react';
 
 const doto = Doto({
   weight: '800',
@@ -7,9 +10,29 @@ const doto = Doto({
 })
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <header className={styles.header}>
       <h1 className={`${doto.className} ${styles.h1}`}>Micro-apps</h1>
+      <button onClick={handleClick} className={styles.menu}>
+        <img src={isOpen ? "/close.webp" : "/open.webp"} className={styles.image}/>
+      </button>
+
+      <nav className={`${styles.nav} ${isOpen ? styles.show : ''}`}>
+        <ul>
+          <li>
+            <a href="/" className={styles.link}>Home</a>
+          </li>
+          <li>
+            <a href="/todo" className={styles.link}>To-do</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
