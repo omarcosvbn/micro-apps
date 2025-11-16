9b918d7775081b25ba12e61fc65d7d22e30a62d2
omarcosvbn <omarcosvbn@gmail.com>
2025-11-16 20:50:23 +0000

    corrected padding when loading a page from the apps bar

--- src/app/dice/page.tsx
+++ src/app/dice/page.tsx
@@ -1,18 +1,20 @@
-"use client"
+'use client'
 
 import Box from '../components/Box/Box'
 import styles from './page.module.scss'
 
 export default function Dice() {
-
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
   }
 
-
   return (
     <div className={styles.container}>
-      <Box cornerImage="/container.webp" mobileCornerImage="/side-container.webp" className={styles.diceBox}>
+      <Box
+        cornerImage="/container.webp"
+        mobileCornerImage="/side-container.webp"
+        className={styles.diceBox}
+      >
         {(() => {
           const roll = Math.floor(Math.random() * 6) + 1
           const src = `/dice/${roll}.webp`
@@ -32,7 +34,9 @@ export default function Dice() {
             className={styles.select}
           >
             <select name="numbers" id="number-select" required defaultValue="guess">
-              <option value="guess" disabled>Guess a number:</option>
+              <option value="guess" disabled>
+                Guess a number:
+              </option>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -44,9 +48,7 @@ export default function Dice() {
         </form>
 
         <button type="submit" form="dice-form" className={styles.submit}>
-          <Box cornerImage="/container.webp">
-            Roll
-          </Box>
+          <Box cornerImage="/container.webp">Roll</Box>
         </button>
       </div>
     </div>
